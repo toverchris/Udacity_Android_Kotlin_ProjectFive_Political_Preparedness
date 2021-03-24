fbad6ad63426e54c74d69695445d51f24441a368
toverchris <christopher.hohm@googlemail.com>
2021-03-24 15:29:57 +0100

    Add the navigation from home screen for the two buttons

--- app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/launch/LaunchFragment.kt
@@ -9,7 +9,9 @@ import androidx.navigation.fragment.findNavController
 import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentLaunchBinding
 import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
-import com.example.android.politicalpreparedness.election.adapter.ElectionListener
+
+// TODO: 3/24/21 has to commented out to build project
+//import com.example.android.politicalpreparedness.election.adapter.ElectionListener
 
 class LaunchFragment : Fragment() {
 
@@ -19,8 +21,8 @@ class LaunchFragment : Fragment() {
         val binding = FragmentLaunchBinding.inflate(inflater)
         binding.lifecycleOwner = this
 
-        binding.representativeButton.setOnClickListener { navToRepresentatives() }
-        binding.upcomingButton.setOnClickListener { navToElections() }
+        binding.buttonFindMyReprentatives.setOnClickListener { navToRepresentatives() }
+        binding.buttonUpcommingElections.setOnClickListener { navToElections() }
 
         return binding.root
     }
